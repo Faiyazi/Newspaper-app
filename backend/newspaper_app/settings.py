1683ab48411843d61eb1d14efef1cf12524f8568
Rifakat Haradwala <rifakat@MacBookAir.lan>
2026-08-24 23:48:17 +0530

    Fix Django admin static files

--- backend/newspaper_app/settings.py
+++ backend/newspaper_app/settings.py
@@ -107,17 +107,18 @@ USE_TZ = True
 
 
 # Static files
-STATIC_URL = "/static/"
 
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
-
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
